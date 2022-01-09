d1aeb6c9a899e06086ed205a489db807a81ed0dc
Thijs Walcarius <thijs.walcarius@ugent.be>
2022-01-09 19:54:06 +0000

    Update frontier_silicon

---
+++ homeassistant/components/frontier_silicon/const.py
@@ -0,0 +1,20 @@
+"""Constants for the Frontier Silicon Media Player integration."""
+
+
+DOMAIN = "frontier_silicon"
+
+CONF_DEVICE_URL = "device_url"
+CONF_PIN = "pin"
+CONF_USE_SESSION = "use_session"
+
+CONF_WEBFSAPI_URL = "webfsapi_url"
+
+
+SERVICE_SELECT_PRESET = "select_preset"
+
+ATTR_PRESET = "preset"
+
+
+DEFAULT_TIMEOUT = 2  # in seconds
+
+DATA_AFSAPI_CLIENT = "client"
